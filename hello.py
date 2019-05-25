54f18555894d1b27916201d96a51588551bb96b0
walker <hiniwoo@163.com>
2019-05-25 21:06:28 +0800

    first commit

---
+++ hello.py
@@ -0,0 +1,14 @@
+#coding=utf-8
+i = 10
+print(i)
+b = 20
+print(b)
+
+print('oihowheoihoi')
+
+print('你好，世界')
+
+print 'hello'
+print 'rooh'
+
+
---
+++ number.py
@@ -0,0 +1,14 @@
+#coding=utf-8
+
+import math
+import cmath
+
+var1 = 1
+
+var2 = 9
+
+var3 = 100
+
+print math.log10(var3)
+
+dir(math)
